243cef8934f015b14425d1b20fb20493c6d27a7d
Adem54 <adem5434e@gmail.com>
2025-05-15 19:00:50 +0200

    update

--- todoApp/src/app/app.component.ts
+++ todoApp/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root',//app-root selector in amaci AppCompnentinin nerde kullanmak istiyorsun..AppComponentini cagirmak icin bir etiket gibi dusunebiliriz!!!!..index.html icerisinde de baktimgzda(<app-root></app-root>) bu selector kullanilarak, AppComponenti nin bir butun olarak, app.component.js,app.component.html,app.component.css dosysinin tamamini cagirilmasini saglamisiz aslnda
   templateUrl: './app.component.html',
   standalone: false,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'todoApp';
+  //Burda app.component.html icerisinde tanimlamak istedigmz degiskenler, veya burdan biz database den gelen bir datayi alip app.compnent.html icinde datayi kullanmak isteyebiliriz...YANI .ts dosyasi bir nevi html dosyasmiz icindeki dinamikligi, ve variablelari {{}} icerisinde kullanabilmemizi sagliyor..
 }
--- todoApp/src/app/app.module.ts
+++ todoApp/src/app/app.module.ts
@@ -5,14 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
+  declarations: [//Compponetnlerin tanimlandigi yer
     AppComponent
   ],
-  imports: [
+  imports: //Modulelerin tanimlandigi yer
+  [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent]//app.module icinde calistirilabilir ilk componenti de burda tanimliyoruz...Ilk calistirilabilecek component, AppComponent olarak tanimlandi..
 })
 export class AppModule { }
+//Bundle -paket olsuturuyoruz...ve bu paketi disari acarak, main.ts icinde app.module i calistirilabilir hale getgiriyoruz
+//Root module app.module diyebiliriz...root component te app.componenttir
+//Burasi angular modulunun, birlesim noktasi...modulleri ve ilk calisacak compnenti de bootstrap key kismninda ekliyoruz
