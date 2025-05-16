e58beef7c6d040e61260739ef17334f13f314521
Adem54 <adem5434e@gmail.com>
2025-05-16 17:02:54 +0200

    update

--- todoApp/src/app/app.component.ts
+++ todoApp/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Model } from './model';
+import { Model, TodoItem } from './model';
 @Component({
   selector: 'app-root',//app-root selector in amaci AppCompnentinin nerde kullanmak istiyorsun..AppComponentini cagirmak icin bir etiket gibi dusunebiliriz!!!!..index.html icerisinde de baktimgzda(<app-root></app-root>) bu selector kullanilarak, AppComponenti nin bir butun olarak, app.component.js,app.component.html,app.component.css dosysinin tamamini cagirilmasini saglamisiz aslnda
   templateUrl: './app.component.html',
@@ -8,6 +8,8 @@ import { Model } from './model';
 })
 export class AppComponent {
  model = new Model();
+ isDisplay = false;
+
  getUserName()
  {
     return this.model.user;
@@ -15,13 +17,36 @@ export class AppComponent {
 
  getItems()
  {
-    return this.model.items;
+    if(this.isDisplay)
+    {
+      return this.model.items;
+    }else 
+    {
+      return this.model.items.filter(item=>!item.completed);
+    }
  }
 
  getItemNotCompleted()
  {
-    return this.model.items.filter(item=>!item.completed);
+    if(this.isDisplay)
+    {
+      return this.model.items;
+    }else 
+    {
+      return this.model.items.filter(item=>!item.completed);
+    }
  }
  //Simdi sunu tam olarak anlayalim...Biz im icin burasi, tum her turlu jscript logiclerini operasyonlarini dogru, okunabilir bir adlandirma ile yapabilecedgimiz bir yerdir....!!!
- 
+
+ addItem(value:string){
+     
+    console.log(`value:  ${value}`);
+    if(value)
+    {
+      let newItem = new TodoItem(this.model.items.length +1, value, false);
+      this.model.items.push(newItem);
+      console.log(`newItem:  ${JSON.stringify(newItem)}`);
+    }
+ }
+
 }
