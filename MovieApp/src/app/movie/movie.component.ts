4cd4fb239d0c4995b5feec5c915a74e5a0aa1d63
Adem54 <adem5434e@gmail.com>
2025-05-17 22:57:23 +0200

    add new compnent with CLI

---
+++ MovieApp/src/app/movie/movie.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'movie',
+  standalone: false,
+  templateUrl: './movie.component.html',
+  styleUrl: './movie.component.css'
+})
+export class MovieComponent {
+
+}
